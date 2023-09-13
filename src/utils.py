149e7cc8e71f2ef367ef8185f4e1e1b2fb4f573b
IrinaAlekseeva <safm.alex@gmail.com>
2023-09-13 10:04:04 +0300

    predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -42,13 +42,9 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
-
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
-
             report[list(models.keys())[i]] = test_model_score
 
         return report
@@ -60,7 +56,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
